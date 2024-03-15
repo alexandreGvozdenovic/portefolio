13150d30e377ae096cbe252b029b33dbda5c12e2
Alexandre Gvozdenovic <alexandregvozdenovic@revolugo.com>
2024-03-15 10:24:09 +0100

    WIP adding translations

---
+++ src/i18n/ui.ts
@@ -0,0 +1,18 @@
+export const languages = {
+  en: "English",
+  fr: "Français",
+};
+export const defaultLang = "fr";
+export const showDefaultLang = false;
+export const ui = {
+  en: {
+    "cv.formations": "Education",
+    "nav.about": "About",
+    "nav.twitter": "Twitter",
+  },
+  fr: {
+    "cv.formations": "Formations",
+    "nav.home": "Accueil",
+    "nav.about": "À propos",
+  },
+} as const;
