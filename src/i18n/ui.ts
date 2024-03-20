53eb9934d04ac5fb41d436eddf51a265a7546d04
Alexandre Gvozdenovic <alexandregvozdenovic@revolugo.com>
2024-03-20 18:37:33 +0100

    finished cv trad

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -6,12 +6,112 @@ export const defaultLang = "fr";
 export const showDefaultLang = false;
 export const ui = {
   en: {
+    "cv.profile": "Profile",
+    "cv.description": "I am toto",
     "cv.formations": "Education",
+    "cv.lacapsule.title":
+      "Coding Bootcamp - Fullstack JavaScript web and mobile developer",
+    "cv.licence.title":
+      "Professional Degree in System and Network Administration",
+    "cv.bts.title": "Advanced computer technician's certificate",
+    "cv.professionalExperience": "Professional experience",
+    "cv.revolugo.title": "Fullstack Developer",
+    "cv.revolugo.subtitle": "May 2023 - Today Révolugo, Paris",
+    "cv.revolugo.description":
+      "In a tech/product team of 9: 1 CTO, 4 fullstack, 2 frontend, 1 product manager and 1 designer.",
+    "cv.revolugo.task.1":
+      "- Update/creation of a Vue2 component library with Storybook, made available to integrators with an API.",
+    "cv.revolugo.task.2":
+      "- Creation of an internal design system with Storybook for components that can be reused by different products.",
+    "cv.revolugo.task.3":
+      "- Migration of legacy products to TypeScript and Vue3 with Nuxt.js.",
+    "cv.revolugo.task.4":
+      "- Creation of migration scripts to update old data that no longer complies with the new data models.",
+    "cv.revolugo.task.5":
+      "- Creation of services to manipulate Sequelize data models.",
+    "cv.cyclofix.title": "Frontend Developer",
+    "cv.cyclofix.subtitle": "June 2021 - April 2023 Cyclofix, Paris",
+    "cv.cyclofix.description":
+      "In a tech team of 6, there is 1 CTO, 2 backend developers, 2 frontend developers, and 1 designer.",
+    "cv.cyclofix.task.1":
+      "- Improvement and maintenance of existing products: two React Native applications and one React web application.",
+    "cv.cyclofix.task.2":
+      "- Development of new features, such as internationalisation using i18n.",
+    "cv.cyclofix.task.3":
+      "- Creation of two new products with NextJs: SaaS for repair workshops, Marketplace for selling second-hand bikes.",
+    "cv.interfast.title": "Fullstack Developer",
+    "cv.interfast.subtitle": "October 2020 - May 2021 Interfast, Paris",
+    "cv.interfast.description":
+      "Creation of a mobile application (React Native) to generate intervention reports for the construction industry",
+    "cv.capsuleteacher.subtitle":
+      "August 2019 - December 2019 Castelis, Ivry-sur-seine",
+    "cv.capsuleteacher.description":
+      "Support for La Capsule students during their 10-week coding bootcamp: explanation of concepts, code review, debugging",
+    "cv.castelis.title": "Project Manager",
+    "cv.castelis.subtitle":
+      "August 2019 - December 2019 Castelis, Ivry-sur-seine",
+    "cv.castelis.description":
+      "In charge of an ERP solution, TMA management for existing customers and project management for new customers",
+    "cv.sncf.subtitle": "September 2017 - June 2019 SNCF Réseau, Paris",
+    "cv.sncf.description": "Incident / Problem Manager within a 5-person team",
     "nav.about": "About",
     "nav.twitter": "Twitter",
   },
   fr: {
+    "cv.profile": "Profil",
+    "cv.description": "Je suis toto",
     "cv.formations": "Formations",
+    "cv.lacapsule.title":
+      "Coding Bootcamp - Développeur web et mobile Fullstack JavaScript",
+    "cv.lacapsule.subtitle": "2020 La Capsule, Paris",
+    "cv.licence.title":
+      "Licence Professionnelle Administration Système et Réseaux",
+    "cv.licence.subtitle":
+      "2016 - 2017 Université Paris-Est Créteil, Lieusaint",
+    "cv.bts.title": "BTS Informatique",
+    "cv.bts.subtitle": "2013 - 2015 Lycée Saint-Aspais, Melun",
+    "cv.professionalExperience": "Expériences Professionnelles",
+    "cv.revolugo.title": "Développeur Fullstack",
+    "cv.revolugo.subtitle": "Mai 2023 - Aujourd'hui Révolugo, Paris",
+    "cv.revolugo.description":
+      "Dans une équipe tech/produit de 9 : 1 CTO, 4 fullstack, 2 frontend, 1 product manager et 1 designer.",
+    "cv.revolugo.task.1":
+      "- Mise à jour / création d’une librairie de composants Vue2 avec Storybook, exposée à des intégrateurs via une API.",
+    "cv.revolugo.task.2":
+      "- Création d’un design system interne avec Storybook pour les composants réutilisable par les différents produits.",
+    "cv.revolugo.task.3":
+      "- Migration des produits “legacy” vers TypeScript et Vue3 avec Nuxt.js.",
+    "cv.revolugo.task.4":
+      "- Création de scripts de migration afin de mettre à jour les anciennes data qui ne respectent plus les nouveaux modèles de données",
+    "cv.revolugo.task.5":
+      "- Création de services servant à manipuler les modèles de données Sequelize.",
+    "cv.cyclofix.title": "Développeur Frontend",
+    "cv.cyclofix.subtitle": "Juin 2021 - Avril 2023 Cyclofix, Paris",
+    "cv.cyclofix.description":
+      "Dans une équipe tech de 6 : 1 CTO, 2 backend, 2 frontend et 1 designer.",
+    "cv.cyclofix.task.1":
+      "- Amélioration et maintenance des produits existants : deux applications react native et une application web react.",
+    "cv.cyclofix.task.2":
+      "- Développement de nouvelles features (ex: internationalisation avec i18n).",
+    "cv.cyclofix.task.3":
+      "- Création de deux nouveaux produits avec NextJs : SaaS pour les ateliers de réparation, Marketplace de vente de vélo d’occasion.",
+    "cv.interfast.title": "Développeur Fullstack",
+    "cv.interfast.subtitle": "Octobre 2020 - Mai 2021 Interfast, Paris",
+    "cv.interfast.description":
+      "Création d’une application mobile (React Native) de génération de rapport d’intervention pour les acteurs du BTP.",
+    "cv.capsuleteacher.title": "Teacher Assistant",
+    "cv.capsuleteacher.subtitle": "Juin 2020 - Décembre 2020 La Capsule, Paris",
+    "cv.capsuleteacher.description":
+      "Accompagnement des élèves de La Capsule durant leurs 10 semaines de coding bootcamp : explication de notions, code review, debugging.",
+    "cv.castelis.title": "Chef de projet",
+    "cv.castelis.subtitle":
+      "Août 2019 - Décembre 2019 Castelis, Ivry-sur-seine",
+    "cv.castelis.description":
+      "En charge d’une solution ERP, gestion de la TMA des clients existants et suivi de projet des nouveaux clients.",
+    "cv.sncf.title": "Incident Manager",
+    "cv.sncf.subtitle": "Septembre 2017 - Juin 2019  SNCF Réseau, Paris",
+    "cv.sncf.description":
+      "Incident / Problème Manager au sein d'une équipe de 5 personnes",
     "nav.home": "Accueil",
     "nav.about": "À propos",
   },
